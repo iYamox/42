9288f3b60ea09ca676b4ea218e184508e199fc07
iYamox <alexandre.mary78360@gmail.com>
2026-05-07 17:50:45 +0200

    philo

--- projet/3eme_cercle/philo/srcs/main.c
+++ projet/3eme_cercle/philo/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: amary <amary@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/03 10:46:19 by amary             #+#    #+#             */
-/*   Updated: 2026/05/07 16:30:35 by amary            ###   ########.fr       */
+/*   Updated: 2026/05/07 17:40:20 by amary            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	main(int argc, char **argv)
 	if (argc != 5 && argc != 6)
 		return (printf("Error: Arguments\n"), 1);
 	if (!init_all(&data, argc, argv))
-		return (1);
+		return (printf("Error: Invalid arguments\n"), 1);
 	if (!start_simulation(&data))
 	{
 		cleanup(&data);
